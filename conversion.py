84d919ee7f21d078a8b55bfa952919f488c839de
Jobin <jobin2600@gmail.com>
2023-05-17 12:44:03 +0200

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,11 +1,11 @@
 def celsius_to_kelvin(celsius):
-    "change to kelvin"
+    #"change to kelvin"
     return celsius + 273.15
 
 def celsius_to_celsius(kelvin):
-    "change to celsius"
-    'better documentation"
-    "even better documentation"
+    #"change to celsius"
+    #'better documentation"
+    #"even better documentation"
     return kelvin - 273.15
 
 def returnWhatComesIn(input):
