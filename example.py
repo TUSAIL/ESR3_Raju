38b52ff4cb71fdd3270fb4ae58b6d5e1bb31c558
Jobin <jobin2600@gmail.com>
2023-05-17 12:24:10 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a + b
+
+def test_add(): # Special name!
+    assert add(2, 3) == 5 # What is assert?
+    assert add('space', 'ship') == 'spaceship'
+
+def subtract(a, b):
+    return a + b  
+
